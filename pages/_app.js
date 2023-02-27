77e094c0692920ccbe309791956f3a68d706469e
Brandon Kowalecki <bkowalecki@gmail.com>
2023-02-27 16:42:07 -0500

    full commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
+import { Provider } from "react-redux";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import store from "../redux/store";
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
   return (
-  <div className="wrapper">
-      <NavBar />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className="wrapper">
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
