855d9a798e54e2dd4228cf23da6735eb23fee7ce
Brandon Kowalecki <bkowalecki@gmail.com>
2023-03-02 12:20:25 -0500

    added toast messgae for cart addition

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,23 +1,34 @@
 import Image from "next/image";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/cart.slice";
+import toast, { Toaster } from "react-hot-toast";
 import styles from "../styles/ProductCard.module.css";
 
 const ProductCard = ({ product }) => {
-  const dispatch = useDispatch();
+  
+    const dispatch = useDispatch();
+
+    const toastAddMsg = () => {toast.success("Added to Cart")};
 
   return (
     <div className={styles}>
-      <Image src={product.image} height={300} width={220} />
+      <Image src={product.image} height={300} width={220} alt="game_image"/>
       <h4 className={styles.title}>{product.product}</h4>
       <h5 className={styles.category}>{product.category}</h5>
       <p>$ {product.price}</p>
       <button
-        onClick={() => dispatch(addToCart(product))}
+        onClick={() => {
+            toastAddMsg();
+            dispatch(addToCart(product));
+        }}
         className={styles.button}
       >
         Add to Cart
       </button>
+      <Toaster 
+        position="bottom-right"
+        reverseOrder={false}
+      />
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,13 @@ import { Provider } from "react-redux";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import store from "../redux/store";
+import toast, { Toaster } from "react-hot-toast";
 import "../styles/globals.css";
+const notify = () => toast("Here is my toast!");
+const sucessNotify = () => toast.success("Sucess!");
+const errorNotify = () => toast.error("Error");
+
+
 
 function App({ Component, pageProps }) {
   return (
